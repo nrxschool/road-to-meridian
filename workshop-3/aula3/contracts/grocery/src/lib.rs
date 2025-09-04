93f4476303d22cc44502ab0adb27b0c245386803
Lucas Oliveira <olivmath@protonmail.com>
2025-09-04 11:07:07 -0300

    feat(contracts): add initial contract implementations for grocery and notepad
    Add base implementations for grocery and notepad contracts with admin functionality, note storage, and versioning. Notepad v2 includes cross-contract call capabilities for note management.

---
+++ workshop-3/aula3/contracts/grocery/src/lib.rs
@@ -0,0 +1,4 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short};
+use soroban_sdk::{Env, String, Symbol};
+
---
+++ workshop-3/aula3/contracts/notepadv1/src/lib.rs
@@ -0,0 +1,87 @@
+#![no_std]
+use soroban_sdk::{contract, contractimpl, contracttype, symbol_short, Env, String, Symbol, Address, BytesN};
+
+const COUNTER: Symbol = symbol_short!("counter");
+
+#[contracttype]
+#[derive(Clone)]
+enum DataKey {
+    Admin,
+}
+
+#[derive(Clone)]
+#[contracttype]
+pub struct Note {
+    pub content: String,
+}
+
+impl Default for Note {
+    fn default() -> Self {
+        Self {
+            content: String::from_str(&Env::default(), ""),
+        }
+    }
+}
+
+#[contract]
+pub struct Notepad;
+
+#[contractimpl]
+impl Notepad {
+    pub fn __constructor(env: Env, admin: Address) {
+        env.storage().instance().set(&DataKey::Admin, &admin);
+        env.storage().instance().set(&COUNTER, &0);
+    }
+}
+
+#[contractimpl]
+impl Notepad {
+    pub fn add_note(env: Env, caller: Address, note: String) {
+        Self::check_admin(env.clone(), caller.clone());
+
+        let note = Note { content: note };
+
+        let counter: i64 = env.storage().instance().get(&COUNTER).unwrap_or(0);
+        env.storage().instance().set(&COUNTER, &(counter + 1));
+
+        env.storage().persistent().set(&counter, &note);
+    }
+
+    pub fn get_note(env: Env, counter: i64) -> Note {
+        env.storage().persistent().get(&counter).unwrap_or_default()
+    }
+
+    pub fn get_counter(env: Env) -> i64 {
+        env.storage().instance().get(&COUNTER).unwrap_or(0)
+    }
+}
+
+#[contractimpl]
+impl Notepad {
+    pub fn check_admin(env: Env, caller: Address) {
+        let admin: Address = env.storage().instance().get(&DataKey::Admin).unwrap();
+        caller.require_auth();
+        assert_eq!(admin, caller);
+    }
+
+    pub fn set_admin(env: Env, caller: Address, new_admin: Address) {
+        Self::check_admin(env.clone(), caller.clone());
+        env.storage().instance().set(&DataKey::Admin, &new_admin);
+    }
+
+    pub fn get_admin(env: Env) -> Address {
+        env.storage().instance().get(&DataKey::Admin).unwrap()
+    }
+}
+
+#[contractimpl]
+impl Notepad {
+    pub fn version() -> u32 {
+        1
+    }
+
+    pub fn upgrade(env: Env, caller: Address, new_wasm_hash: BytesN<32>) {
+        Self::check_admin(env.clone(), caller.clone());
+        env.deployer().update_current_contract_wasm(new_wasm_hash);
+    }
+}
