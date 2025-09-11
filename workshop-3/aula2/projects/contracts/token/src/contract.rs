1e352c819038cd99e5e87fa3aa5d05cab4ec401a
Lucas Oliveira <olivmath@protonmail.com>
2025-09-11 15:17:37 -0300

    feat(token): add ownership and minting functionality to token contract
    - Add Ownable trait implementation for access control
    - Include mint function with owner-only restriction
    - Update constructor to initialize owner and mint initial supply

--- workshop-3/aula2/projects/contracts/token/src/contract.rs
+++ workshop-3/aula2/projects/contracts/token/src/contract.rs
@@ -1,27 +1,39 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Stellar Soroban Contracts ^0.4.1
 
-use soroban_sdk::{contract, contractimpl, Env, String};
-use stellar_macros::default_impl;
+use soroban_sdk::{Address, contract, contractimpl, Env, String};
+use stellar_access::ownable::{self as ownable, Ownable};
+use stellar_macros::{default_impl, only_owner};
 use stellar_tokens::fungible::{Base, FungibleToken};
 
 #[contract]
-pub struct MyToken;
+pub struct Meridian;
 
 #[contractimpl]
-impl MyToken {
-    pub fn __constructor(e: &Env) {
-        Base::set_metadata(
-            e,
-            18,
-            String::from_str(e, "Meridian"),
-            String::from_str(e, "MERN"),
-        );
+impl Meridian {
+    pub fn __constructor(e: &Env, recipient: Address, owner: Address) {
+        Base::set_metadata(e, 18, String::from_str(e, "Meridian"), String::from_str(e, "MERIDIAN2"));
+        Base::mint(e, &recipient, 100000000000000000000000000);
+        ownable::set_owner(e, &owner);
+    }
+
+    #[only_owner]
+    pub fn mint(e: &Env, account: Address, amount: i128) {
+        Base::mint(e, &account, amount);
     }
 }
 
 #[default_impl]
 #[contractimpl]
-impl FungibleToken for MyToken {
+impl FungibleToken for Meridian {
     type ContractType = Base;
+
 }
+
+//
+// Utils
+//
+
+#[default_impl]
+#[contractimpl]
+impl Ownable for Meridian {}
