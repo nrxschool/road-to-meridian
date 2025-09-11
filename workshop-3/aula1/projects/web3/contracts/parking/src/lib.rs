7f9b4d86949666fa4d616015506d70d120142e70
Lucas Oliveira <olivmath@protonmail.com>
2025-09-11 11:28:39 -0300

    update

--- workshop-3/aula1/projects/web3/contracts/parking/src/lib.rs
+++ workshop-3/aula1/projects/web3/contracts/parking/src/lib.rs
@@ -1,6 +1,21 @@
 #![no_std]
+use soroban_sdk::{contract, contractimpl, contracttype, Address, Env};
 
-use soroban_sdk::{contract, contractimpl};
+#[contracttype]
+#[derive(PartialEq, Eq)]
+pub enum Status {
+    Checkin,
+    Checkout,
+    Wiched,
+}
+
+#[contracttype]
+pub struct Ticket {
+    pub owner: Address,
+    pub start: u32,
+    pub end: u32,
+    pub status: Status,
+}
 
 #[contract]
 pub struct ParkingContract;
@@ -8,4 +23,60 @@ pub struct ParkingContract;
 #[contractimpl]
 impl ParkingContract {
     pub fn __constructor() {}
+
+    // fn pay_validation() -> bool {
+    //     // 2. Token.allowance(user, 100);
+    //     // 3. Token.transferFrom(user, ParkingContractAddress, 100);
+    //     return true;
+    // }
+
+    // 1. User Token.approve(ParkingContractAddress, 100);
+    pub fn store(env: Env, id: u32, user: Address) {
+        // if Self::pay_validation() == false {
+        //     return symbol_short!("naopagou")
+        // }
+
+        user.require_auth();
+
+        let ticket = Ticket {
+            owner: user,
+            start: env.ledger().sequence(),
+            end: env.ledger().sequence() + 60,
+            status: Status::Checkin,
+        };
+        env.storage().temporary().set(&id, &ticket);
+        env.storage()
+            .temporary()
+            .extend_ttl(&id, 10, env.ledger().sequence() + 60);
+    }
+
+    pub fn get(env: Env, id: u32) -> Status {
+        match env.storage().temporary().get::<u32, Ticket>(&id) {
+            Some(ticket) => ticket.status,
+            None => Status::Wiched,
+        }
+    }
+
+    pub fn collect(env: Env, id: u32, user: Address) -> Status {
+        user.require_auth();
+
+        let ticket = env.storage().temporary().get::<u32, Ticket>(&id);
+
+
+        match ticket {
+            Some(t) => {
+                if t.status == Status::Checkin {
+                    let ticket2 = Ticket {
+                        owner: t.owner,
+                        start: t.start,
+                        end: t.end,
+                        status: Status::Checkout,
+                    };
+                    env.storage().temporary().set(&id, &ticket2);
+                }
+                Status::Checkout
+            }
+            None => Status::Wiched,
+        }
+    }
 }
